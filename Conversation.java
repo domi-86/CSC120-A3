7718ce9aacdaaa96f8933afdf393f624ddb8d905
Dominique Guillory <dguillory@smith.edu>
2024-09-25 13:09:19 -0400

    1st version: transcript is not in an array

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,116 @@
+import java.util.*;
+
 class Conversation {
 
+
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("How many rounds? ");
+    int numRounds = input.nextInt();
+    input.nextLine();
+
+    String transcript = "Hi there!  What's on your mind?";
+
+    System.out.println("Hi there!  What's on your mind?");
+    // String userResponse = input.nextLine();
+    // updates the transcript with the user's response and the bot's response
+    // transcript = transcript + "\n" + userResponse + "\n" + bot(userResponse);
+
+    // starts a loop for the amount of rounds
+    for (int i = 0; i < numRounds; i++) {  
+      
+      String nextResponse = input.nextLine();
+      transcript = transcript + "\n" + nextResponse;
+      String botResponse = bot(nextResponse);
+      transcript = transcript + "\n" + botResponse;
+
+
+    }
+
+    System.out.println("Goodbye!");
+
+    transcript = transcript + "\n" + "Goodbye!";
+
+    input.close();
+
+    System.out.println("TRANSCRIPT: \n" + transcript);
+
   }
-}
+
+
+    //mirrors a response or returns a random response
+    public static String bot(String response) {
+
+      String botResponse = "";
+      String[] ranResponses = {"So interesting!", "Mmm-hm.", "Tell me more!", "I see."};
+      Random rand = new Random();
+
+      //if the user input contains any of these words that can be mirrored, replace them. otherwise, provide a random response
+      if (response.contains("I") || response.contains("I'm") || response.contains("you") || response.contains("my") ||
+      response.contains("me") || response.contains("your") || response.contains("am") ) {
+
+        //mirrors I'm
+        if (response.contains("I'm")) {
+          if (response.indexOf("I'm") != 0) {
+            botResponse = response.replace("I'm", "you're");
+          }
+          else {
+            botResponse = response.replace("I'm", "You're");
+          }
+        }
+        // mirrors I and me
+        if (response.contains(" I ")) {
+          botResponse = response.replace("I","you");
+        }
+        if (response.contains("I") && response.indexOf("I") == 0) {
+          botResponse = response.replace("I","You");
+        }
+        if (response.contains(" me ")) {
+          botResponse = response.replace("me","you");
+        }
+        //mirrors am
+        if (response.contains(" am ")) {
+          botResponse = response.replace("am","are");
+        }
+        //mirrors are
+        if (response.contains(" are ")) {
+          botResponse = response.replace("are","with");
+        }
+        //mirrors you
+        if (response.contains(" you ")) {
+          botResponse = response.replace("you","I");
+        }
+        //mirrors my
+        if (response.contains(" my ")) {
+          botResponse = response.replace("my","your");
+        }
+        //mirrors your
+        if (response.contains(" your ")) {
+          botResponse = response.replace("your","my");
+        }
+
+      botResponse = botResponse.replace(".","?");
+      System.out.println(botResponse);
+      return botResponse;
+      }
+
+      //choose random response from array
+      else {
+
+        botResponse = ranResponses[rand.nextInt(ranResponses.length-1)];
+
+        System.out.println(botResponse);
+
+        return botResponse;
+
+      }
+
+    }
+
+
+    // You will start the conversation here.
+
+   
+  }
+
--- reflection.md
+++ reflection.md
@@ -1,7 +1,17 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    I did not consult any classmates yet, aside from taking note of problems in class. I also used an origami crane.
 - Which session(s) of TA / office hours did you attend?
+    I did not attend TA/office hours yet.
 - What are your initial impressions of Java? 
+    It isn't too hard yet, but I am still learning.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    Some things are better done object-oriented, some things aren't. These two languages are made to be effiecient and there are ways to save space.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    I tried using python syntax, """ """ string but that didn't work, so I made a String object and kept concactenating to it and adding a new line manually. My first solution for the transcript was to make an array and put each line in a spot, then print the array, but then I thought that was probably too complicated, but then I saw that that was what I am supposed to do.
+    Initially, replacing the period with a question mark, I was trying to do "response.replace(".","?") at the end, which is wrong because I made a variable for the mirrored version. I doubt the way I did it is most efficient, but making use of substrings and String methods is very helpful. 
+
+
+Current Problem: won't mirror all words, only 1
+example "I'm thinking about my class" gets mirrored to "I'm thinking about your class?"
