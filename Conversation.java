c8e5da1ce49bdfcc5cc160e889a6f9a35a66b371
Dominique Guillory <dguillory@smith.edu>
2024-10-08 12:00:33 -0400

    fixed bugs and formatting

--- Conversation.java
+++ Conversation.java
@@ -2,85 +2,94 @@ import java.util.*;
 
 class Conversation {
 
-  /** Array of strings to hold the conversation */
-  private final String[] transcriptArr;
-  /** Number of rounds in the conversation */
-  private static int numRounds;
+    /** Array of strings to hold the conversation */
+    private final String[] transcriptArr;
+    /** Number of rounds in the conversation */
+    private static int numRounds;
+    private static String[] ranResponses;
 
-  /**
+
+    /**
      * Constructor
      * @param numRounds the number of rounds of conversation
      */
-  public Conversation(int numRounds) {
-    this.transcriptArr = new String[(numRounds*2)+2];
-    Conversation.numRounds = numRounds;
-  }
-
-  public static int getRounds() {
-    return numRounds;
-  }
-
-
-  /**
-   * main method, asks for the number of rounds, then creates a Conversation object
-   * and initiates the conversation
-   * @param arguments
-   */
-  public static void main(String[] arguments) {
-
-    Scanner s = new Scanner(System.in);
-
-    System.out.println("How many rounds? ");
-    int numberRounds = s.nextInt();
-    s.nextLine();
-
-   
-    Conversation c = new Conversation(numberRounds);
-
-    System.out.println(c.convo());
-
-    s.close();
-
-  }
-
-  /**
-   * convo
-   * Contains the entire conversation
-   * @return returns the transcript of the conversation
-   */
-    public String convo() {
+    public Conversation(int numRounds) {
+        this.transcriptArr = new String[(numRounds * 2) + 2];
+        this.numRounds = numRounds;
+        this.ranResponses = new String[] {
+            "So interesting!",
+            "Mmm-hm.",
+            "Tell me more!",
+            "I see."
+        };
+    }
 
 
-      Scanner input = new Scanner(System.in);
-  
-
-      System.out.println("Hi there!  What's on your mind?");
-      transcriptArr[0] = "Hi there!  What's on your mind?";
+    public static int getRounds() {
+        return numRounds;
+    }
 
 
-       /**
-     * starts a loop for the amount of rounds, takes in a response, mirrors it until end of loop
+    /**
+     * main method, asks for the number of rounds, then creates a Conversation object
+     * and initiates the conversation
+     * @param arguments
      */
-    for (int i = 0, j = 1; i < numRounds; i++, j+=2) {  
-      
-      String nextResponse = input.nextLine();
-      transcriptArr[j] = nextResponse;
+    public static void main(String[] arguments) {
 
-      String botResponse = bot(nextResponse);
-      transcriptArr[j+1] = botResponse;
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("How many rounds? ");
+        int numberRounds = scan.nextInt();
+        scan.nextLine();
+
+
+        Conversation c = new Conversation(numberRounds);
+
+        System.out.println(c.convo());
+
+        scan.close();
 
     }
 
-    System.out.println("Goodbye!");
-    transcriptArr[(1+numRounds*2)] = "Goodbye!";
+    /**
+     * convo
+     * Contains the entire conversation
+     * @return returns the transcript of the conversation
+     */
+    public String convo() {
 
-    input.close();
 
-    // convert array of strings into string
-    return "TRANSCRIPT:\n"+getTranscript();
-  
-      
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Hi there!  What's on your mind?");
+        transcriptArr[0] = "Hi there!  What's on your mind?";
+
+
+        /**
+         * starts a loop for the amount of rounds, takes in a response, mirrors it until end of loop
+         */
+        for (int i = 0, j = 1; i < numRounds; i++, j += 2) {
+
+            String nextResponse = input.nextLine();
+            transcriptArr[j] = nextResponse;
+
+            String botResponse = bot(nextResponse);
+            transcriptArr[j + 1] = botResponse;
+
+
+        }
+
+        System.out.println("Goodbye!");
+        transcriptArr[(1 + numRounds * 2)] = "Goodbye!";
+
+        input.close();
+
+        // convert array of strings into string
+        return "TRANSCRIPT:\n" + getTranscript();
+
+
     }
 
 
@@ -92,85 +101,80 @@ class Conversation {
      */
     public static String bot(String response) {
 
-      String botResponse = response;
-      String[] ranResponses = {"So interesting!", "Mmm-hm.", "Tell me more!", "I see."};
-      Random rand = new Random();
+        String botResponse = response;
+        Random rand = new Random();
 
-      //if the user input contains any of these words that can be mirrored, replace them. otherwise, provide a random response
-      if (response.contains("I ") || response.contains("I'm") || response.contains("you") || response.contains("my") ||
-      response.contains("me") || response.contains("your") || response.contains(" am") ) {
+        //if the user input contains any of these words that can be mirrored, replace them. otherwise, provide a random response
+        if (response.contains("I") || response.contains("I'm") || response.contains("you") || response.contains("my") ||
+            response.contains("me") || response.contains("your") || response.contains(" am")) {
 
-        //mirrors I'm
-        if (response.contains("I'm")) {
-          if (response.indexOf("I'm") != 0) {
-            botResponse = botResponse.replace("I'm", "you're");
-          }
-          if (response.startsWith("I'm")) {
-            botResponse = botResponse.replace("I'm", "You're");
-          }
-        }
-        // mirrors I and me
-        if (response.contains("I ") && response.indexOf("I ") != 0) {
-          botResponse = botResponse.replace("I","you");
-        }
-        if (response.contains("I ") && response.startsWith("I ")) {
-          botResponse = botResponse.replace("I","You");
-        }
-        if (response.contains(" me ")) {
-          botResponse = botResponse.replace("me","you");
-        }
-        //mirrors am
-        if (response.contains(" am")) {
-          botResponse = botResponse.replace("am","are");
-        }
-        //mirrors are
-        if (response.contains(" are ")) {
-          botResponse = botResponse.replace("are","with");
-        }
-        //mirrors you
-        if (response.contains(" you ")) {
-          botResponse = botResponse.replace("you","I");
-        }
-        //mirrors my
-        if (response.contains(" my ")) {
-          botResponse = botResponse.replace("my","your");
-        }
-        //mirrors your
-        if (response.contains(" your ")) {
-          botResponse = botResponse.replace("your","my");
+            //mirrors I'm
+            if (response.contains("I'm")) {
+                if (response.indexOf("I'm") != 0) { //if I'm is not at the beginning of the sentence
+                    botResponse = botResponse.replace("I'm", "you're");
+                }
+                if (response.startsWith("I'm")) {
+                    botResponse = botResponse.replace("I'm", "You're");
+                }
+            }
+            // mirrors I and me
+            if (response.contains("I ") && response.indexOf("I ") != 0) {
+                botResponse = botResponse.replace("I", "you");
+            }
+            if (response.contains("I ") && response.startsWith("I ")) {
+                botResponse = botResponse.replace("I", "You");
+            }
+            if (response.contains(" me ")) {
+                botResponse = botResponse.replace("me", "you");
+            }
+            //mirrors am
+            if (response.contains(" am")) {
+                botResponse = botResponse.replace("am", "are");
+            }
+            //mirrors are
+            if (response.contains(" are ")) {
+                botResponse = botResponse.replace("are", "am"); //change this
+            }
+            //mirrors you
+            if (response.contains(" you ")) {
+                botResponse = botResponse.replace("you", "I");
+            }
+            //mirrors my
+            if (response.contains(" my ")) {
+                botResponse = botResponse.replace("my", "your");
+            }
+            //mirrors your
+            if (response.contains(" your ")) {
+                botResponse = botResponse.replace("your", "my");
+            }
+
+            botResponse = botResponse.replace(".", "?");
+            System.out.println(botResponse);
+            return botResponse;
         }
 
-      botResponse = botResponse.replace(".","?");
-      System.out.println(botResponse);
-      return botResponse;
-      }
+        //choose random response from array
+        else {
 
-      //choose random response from array
-      else {
+            botResponse = ranResponses[rand.nextInt(ranResponses.length - 1)];
 
-        botResponse = ranResponses[rand.nextInt(ranResponses.length-1)];
+            System.out.println(botResponse);
 
-        System.out.println(botResponse);
+            return botResponse;
 
-        return botResponse;
-
-      }
+        }
 
     }
 
     public String getTranscript() {
-      String transcript = "";
-      for (String line : transcriptArr) {
-          transcript = transcript + line + "\n";
-      }
+        String transcript = "";
+        for (String line: transcriptArr) {
+            transcript = transcript + line + "\n";
+        }
 
-      return transcript;
+        return transcript;
 
     }
 
 
-    // You will start the conversation here.
-
-   
-  }
-
+}
